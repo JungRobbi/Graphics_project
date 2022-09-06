125bcb442deaee511782af9a0a69e6eb0f6f9a16
JungRobbi <84313999+JungRobbi@users.noreply.github.com>
2022-09-07 02:13:10 +0900

    코드 정리

--- MainProject/GameScene.cpp
+++ MainProject/GameScene.cpp
@@ -39,12 +39,12 @@ GameScene::GameScene(int num_scene, int* index_list, GLuint* tex, GLuint* vao, G
 		{
 			CreateSkyBox(index_list, tex, vao);
 
-			//auto star = CreateStar(index_list,tex,vao);
+			auto star = CreateStar(index_list,tex,vao);
 
-			//star->GetComponent<Transform3D>()->position = glm::vec3(0.0f, 5.5f, 0.0f);
-			//star->GetComponent<Transform3D>()->scale = glm::vec3(1.0f, 0.3f, 1.0f);
-			//star->GetComponent<Transform3D>()->direction = glm::vec3(0.0f, 1.0f, 0.0f);
-			//star->GetComponent<Transform3D>()->roll = 90.0f;
+			star->GetComponent<Transform3D>()->position = glm::vec3(0.0f, 5.5f, 0.0f);
+			star->GetComponent<Transform3D>()->scale = glm::vec3(1.0f, 0.3f, 1.0f);
+			star->GetComponent<Transform3D>()->direction = glm::vec3(0.0f, 1.0f, 0.0f);
+			star->GetComponent<Transform3D>()->roll = 90.0f;
 		}
 	}
 	else if (num_scene == 2) {
